ca45db440fb820c1e7edd9a0525b0cc34239f971
dawidowskilukasz <s22621@pjwstk.edu.pl>
2024-01-20 14:59:55 +0100

    revert changes

--- LAB_3/tests/basic_game_test.py
+++ LAB_3/tests/basic_game_test.py
@@ -35,23 +35,23 @@ class TestMazeGame(unittest.TestCase):
                 output = buf.getvalue()
         self.assertIn("Objective", output)
 
-    # def test_check_valid_move_up(self):
-    #     initial_position = self.game.current_position
-    #     self.game.move('w')
-    #     expected_position = (initial_position[0] - 1, initial_position[1])
-    #     self.assertEqual(self.game.current_position, expected_position)
-    #
-    # def test_check_valid_move_down(self):
-    #     initial_position = self.game.current_position
-    #     self.game.move('s')
-    #     expected_position = (initial_position[0] + 1, initial_position[1])
-    #     self.assertEqual(self.game.current_position, expected_position)
-    #
-    # def test_check_valid_move_right(self):
-    #     initial_position = self.game.current_position
-    #     self.game.move('d')
-    #     expected_position = (initial_position[0], initial_position[1] + 1)
-    #     self.assertEqual(self.game.current_position, expected_position)
+    def test_check_valid_move_up(self):
+        initial_position = self.game.current_position
+        self.game.move('w')
+        expected_position = (initial_position[0] - 1, initial_position[1])
+        self.assertEqual(self.game.current_position, expected_position)
+
+    def test_check_valid_move_down(self):
+        initial_position = self.game.current_position
+        self.game.move('s')
+        expected_position = (initial_position[0] + 1, initial_position[1])
+        self.assertEqual(self.game.current_position, expected_position)
+
+    def test_check_valid_move_right(self):
+        initial_position = self.game.current_position
+        self.game.move('d')
+        expected_position = (initial_position[0], initial_position[1] + 1)
+        self.assertEqual(self.game.current_position, expected_position)
 
     def test_check_invalid_move(self):
         old_position = self.game.current_position
@@ -66,19 +66,19 @@ class TestMazeGame(unittest.TestCase):
         self.assertEqual(self.game.current_position, (0, 0))
         mocked_print.assert_called_with("!! You cannot leave the board.\n")
 
-    # def test_check_obstacle_move(self):
-    #     self.game = MazeGame(size=5, obstacles=[(0, 0), (1, 0), (2, 1), (3, 3), (4, 1)], start=(2, 0), end=(2, 4))
-    #     new_position = (3, 0)
-    #
-    #     if self.game.start[0] < new_position[0]:
-    #         direction = 'w'
-    #     else:
-    #         direction = 's'
-    #
-    #     with patch('builtins.print') as mocked_print:
-    #         self.game.move(direction)
-    #
-    #     mocked_print.assert_called_with("!! You cannot move onto an obstacle.\n")
+    def test_check_obstacle_move(self):
+        self.game = MazeGame(size=5, obstacles=[(0, 0), (1, 0), (2, 1), (3, 3), (4, 1)], start=(2, 0), end=(2, 4))
+        new_position = (3, 0)
+
+        if self.game.start[0] < new_position[0]:
+            direction = 'w'
+        else:
+            direction = 's'
+
+        with patch('builtins.print') as mocked_print:
+            self.game.move(direction)
+
+        mocked_print.assert_called_with("!! You cannot move onto an obstacle.\n")
 
     def test_check_game_finish(self):
         game = MazeGame(5)
